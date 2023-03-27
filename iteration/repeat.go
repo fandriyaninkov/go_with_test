ec8048e970c1032c55c3b850c9d55d2ea70369b9
fandriyaninkov <fandriyaninkov@gmail.com>
2023-03-27 21:16:20 +0300

    add example for iteration

---
+++ iteration/repeat.go
@@ -0,0 +1,11 @@
+package iteration
+
+// Возвращает строку повторяющихся символов.
+func Repeat(character string, count int) string {
+	var repeated string
+	for i := 0; i < count; i++ {
+		repeated += character
+	}
+
+	return repeated
+}
